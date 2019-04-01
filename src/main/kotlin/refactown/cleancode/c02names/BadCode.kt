e833dba2041dca525b4a5b4723ba91fd7f462981
07311631769 <Eletron@1>
2019-04-01 09:54:53 -0300

    names code

---
+++ src/main/kotlin/refactown/cleancode/c02names/BadCode.kt
@@ -0,0 +1,44 @@
+package refactown.cleancode.c02names
+
+import java.lang.reflect.Field
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
+
+// Nomes genéricos
+val d: Int = 5 // tempo decorrido em dias
+val list = ArrayList<Field>() // field list
+fun func(n: Int){
+    for (i in 1..10)
+        if (n == 3)(println("count:$i = 3"))
+}
+
+// Nomes não pronunciáveis
+val genymdhms=  LocalDate.now();
+val modymdhms=  LocalDate.now();
+val pszqint = 102;
+
+// Número Mágico - difícil de encontrar depois
+fun podeSerEireli(socios: Int) = socios <= 3
+
+// Notação Húngara
+val strNome = "Fulano"
+val intIdade = 30
+val dtNascimento = LocalDate.of(1990,Month.APRIL, 12)
+
+// Etc
+val pessoa : IPessoa = Pessoa()
+val enderecoDeCobrancaDoCliente= EnderecoDoCliente()
+val enderecoDeEntregaDoCliente= EnderecoDoCliente()
+
+
+
+
+
+
+
+class EnderecoDoCliente
+private interface IPessoa
+open class Pessoa: IPessoa
+
+
---
+++ src/main/kotlin/refactown/cleancode/c02names/Refactoring1.kt
@@ -0,0 +1,45 @@
+package refactown.cleancode.c02names
+
+import java.lang.reflect.Field
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
+
+// Nomes Significativos
+val tempoDecorrido: Int = 5
+val fields = ArrayList<Field>()
+fun imprimirSoNoTres(numero: Int){
+    for (i in 1..10)
+        if (numero == PRINT) println("count:$i = $PRINT")
+}
+
+// Nomes pronunciáveis
+val generationTimestamp=  LocalDate.now()
+val modificationTimestamp=  LocalDate.now()
+val recordId = 102;
+
+// Constantes para números mágicos e afins
+fun isEireli(socios: Int) = socios <= MAX_SOCIOS_EIRELI
+
+// Sem Notação Húngara
+val nome = "Fulano"
+val idade = 30
+val nascimento = LocalDate.of(1990,Month.APRIL, 12)
+
+// Etc
+val pessoa : Pessoa = PessoaImpl()
+val enderecoCobranca= Endereco()
+val enderecoEntrega= Endereco()
+
+
+
+
+
+
+val PRINT = 3
+val MAX_SOCIOS_EIRELI = 3
+
+class Endereco
+class PessoaImpl: Pessoa()
+
+
